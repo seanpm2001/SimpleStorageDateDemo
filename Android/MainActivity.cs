421f3a15b239136a5ce787f8c5fea769d91c6adb
Rodja Trappe <mail@rodja.net>
2014-08-30 07:50:46 +0200

    demo project

---
+++ Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+using PerpetualEngine.Storage;
+
+
+namespace SimpleStorageDateDemo.Android
+{
+    [Activity(Label = "SimpleStorageDateDemo.Android.Android", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : AndroidActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SimpleStorage.SetContext(this);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+
+            SetPage(App.GetMainPage());
+
+
+            double expiresIn = 3600;
+            var storage = SimpleStorage.EditGroup("mystore");
+            DateTime futureTime = DateTime.Now.AddSeconds(expiresIn);
+            storage.Put<DateTime>("authkey-expire", futureTime);
+            DateTime expireDate = storage.Get<DateTime>("authkey-expire");
+
+            Console.WriteLine("now: " + DateTime.Now);
+            Console.WriteLine("expires: " + expireDate);
+        }
+    }
+}
+
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Xamarin.Forms;
+using PerpetualEngine.Storage;
+
+namespace SimpleStorageDateDemo.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        UIWindow window;
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
+
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
+            
+            window.RootViewController = App.GetMainPage().CreateViewController();
+            window.MakeKeyAndVisible();
+
+            double expiresIn = 3600;
+            var storage = SimpleStorage.EditGroup("mystore");
+            DateTime futureTime = DateTime.Now.AddSeconds(expiresIn);
+            storage.Put<DateTime>("authkey-expire", futureTime);
+            DateTime expireDate = storage.Get<DateTime>("authkey-expire");
+
+            Console.WriteLine("now: " + DateTime.Now);
+            Console.WriteLine("expires: " + expireDate);
+
+            return true;
+        }
+    }
+}
+
